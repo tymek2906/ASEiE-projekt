f123c3c08b1631874116757d4a3cab9fc0136c1c
tymek <tymek29060@gmail.com>
2022-09-11 17:36:11 +0200

    fixes for aws emr work

--- link_reader.py
+++ link_reader.py
@@ -1,9 +1,6 @@
-
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as f
 from pyspark.sql.types import StructType, StructField, StringType
-import requests
-import gzip
 
 
 class LinkReader:
@@ -31,10 +28,14 @@ class LinkReader:
                          r"crawl-data/CC-MAIN-2018-17/warc.paths.gz"]
         self._url2019 = [r"crawl-data/CC-MAIN-2019-13/warc.paths.gz",
                          r"crawl-data/CC-MAIN-2019-18/warc.paths.gz"]
-        self._url2018_index = [r"crawl-data/CC-MAIN-2018-13/cc-index.paths.gz",
-                               r"crawl-data/CC-MAIN-2018-17/cc-index.paths.gz"]
-        self._url2019_index = [r"crawl-data/CC-MAIN-2019-13/cc-index.paths.gz",
-                               r"crawl-data/CC-MAIN-2019-18/cc-index.paths.gz"]
+        self._url2018_index = [r"s3://commoncrawl/cc-index/collections/CC-MAIN-2018-13/indexes/",
+                               r"s3://commoncrawl/cc-index/collections/CC-MAIN-2018-17/indexes/"]
+        self._url2019_index = [r"s3://commoncrawl/cc-index/collections/CC-MAIN-2019-13/indexes/",
+                               r"s3://commoncrawl/cc-index/collections/CC-MAIN-2019-18/indexes/"]
+        self._urlcluster = [r"s3://commoncrawl/cc-index/collections/CC-MAIN-2018-13/indexes/cluster.idx",
+                            r"s3://commoncrawl/cc-index/collections/CC-MAIN-2018-17/indexes/cluster.idx",
+                            r"s3://commoncrawl/cc-index/collections/CC-MAIN-2019-13/indexes/cluster.idx",
+                            r"s3://commoncrawl/cc-index/collections/CC-MAIN-2019-18/indexes/cluster.idx"]
 
     def get_indexes(self):
         return self._indexes
@@ -57,7 +58,7 @@ class LinkReader:
                 f.col("name") == '',
                 f.regexp_extract(f.col('_c2'), r"""(.*)\s(cdx\-[0-9]+\.gz)(.*)""", 2)
             ).
-            otherwise(f.col("name")))
+                otherwise(f.col("name")))
 
         df = df.withColumn(
             "name",
@@ -65,7 +66,7 @@ class LinkReader:
                 f.col("name") == '',
                 f.regexp_extract(f.col('_c3'), r"""(.*)\s(cdx\-[0-9]+\.gz)(.*)""", 2)
             ).
-            otherwise(f.col("name")))
+                otherwise(f.col("name")))
 
         df = df.dropDuplicates(['name'])
         df = df.filter((df.name != '') & (df.name != 'none'))
@@ -80,8 +81,8 @@ class LinkReader:
     def index_reader(self, file_location, as_df=False):
         df = self._spark.read.text(file_location)
 
-        df = df.select(f.col('value').substr(1,2).alias('pl'),
-                       f.regexp_extract(f.col('value'),"""filename.*charset""",0).alias('value')).\
+        df = df.select(f.col('value').substr(1, 2).alias('pl'),
+                       f.regexp_extract(f.col('value'), """filename.*charset""", 0).alias('value')). \
             groupBy(f.col('value'), f.col('pl')).count()
 
         df = df.filter(f.col('pl').rlike(r"""pl"""))
@@ -116,9 +117,8 @@ class LinkReader:
         return self._url_s3 + url
 
     def get_file(self, url):
-        file = requests.get(url)
-        file_content_bytes = gzip.decompress(file.content)
-        file_content_list = file_content_bytes.decode('utf-8').strip().split('\n')
+        # file_content_bytes = gzip.decompress(url)
+        file_content_list = url.strip().split('\n')
         return file_content_list
 
     def get_cluster(self, list_of_urls):
@@ -127,27 +127,43 @@ class LinkReader:
         except IndexError:
             print("This isn't a list of urls containing a cluster")
 
-    def process(self, idx_url):
-        index_file_link = self.get_full_url_s3(idx_url)  # Get link to s3 index file
-        index_file_downloaded = self.get_file(index_file_link)  # Download it to find the cluster file
-        cluster_file = self.get_cluster(index_file_downloaded)  # Get link to s3 cluster file
-        self.cluster_reader(cluster_file)  # Read cluster file and find indexes of files in index file containing pl
-        self.index_paths_reader(index_file_link)  # Get full links to those index files
-        for full_index in self._full_indexes:  # Iterate over the index files
-            self.index_reader(full_index)  # Retrieve links to warc files and put them into self._filenames
+    def get_full_url(self, l, url):
+        i = []
+        for x in l:
+            i.append(url + x)
+        return i
+
+    def process(self, idx_url, cluster_id):
+        # index_file_link = self.get_full_url_s3(idx_url)  # Get link to s3 index file
+        # index_file_downloaded = self.get_file(index_file_link)  # Download it to find the cluster file
+        # cluster_file = self.get_cluster(index_file_downloaded)  # Get link to s3 cluster file
+        self.cluster_reader(cluster_id)  # Read cluster file and find indexes of files in index file containing pl
+        index_file_list = self.get_full_url(self._indexes, idx_url)
+        # self.index_paths_reader(index_file_list)  # Get full links to those index files
         self._indexes = set()  # reset short indexes
         self._full_indexes = set()  # reset full indexes
+        return index_file_list
 
     def find_warcs(self):
-        self.process(self._url2018_index[0])
-        self.process(self._url2018_index[1])
-        self.process(self._url2019_index[0])
-        self.process(self._url2019_index[1])
+        files = []
+        files1 = self.process(self._url2018_index[0], self._urlcluster[0])
+        files2 = self.process(self._url2018_index[1], self._urlcluster[1])
+        files3 = self.process(self._url2019_index[0], self._urlcluster[2])
+        files4 = self.process(self._url2019_index[1], self._urlcluster[3])
+        for x in files1:
+            files.append(x)
+        for x in files2:
+            files.append(x)
+        for x in files3:
+            files.append(x)
+        for x in files4:
+            files.append(x)
+        self.index_reader(files)  # Retrieve links to warc files and put them into self._filenames
 
-if __name__ == '__main__':
 
-    spark = SparkSession.builder.config("spark.driver.memory", "15g").\
-        config("spark.driver.maxResultSize", "4g").appName("SparkProject").getOrCreate()
-    lr = LinkReader(spark)
-    lr.find_warcs()
-    lr.get_filenames()
+spark = SparkSession.builder.config("spark.driver.memory", "15g").config("spark.driver.maxResultSize", "4g").appName(
+    "SparkProject").getOrCreate()
+lr = LinkReader(spark)
+lr.find_warcs()
+print(len(lr.get_filenames()))
+spark.stop()
